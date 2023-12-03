9e39548249b01ef03099009625afa89bf1aa76b7
Tasi <balazstasi2@gmail.com>
2023-12-03 15:59:17 +0100

    Day 3 - part 1

--- years/2023/03/index.ts
+++ years/2023/03/index.ts
@@ -1,9 +1,9 @@
-import _ from "lodash";
-import * as util from "../../../util/util";
-import * as test from "../../../util/test";
-import chalk from "chalk";
-import { log, logSolution, trace } from "../../../util/log";
-import { performance } from "perf_hooks";
+import _, { isNumber, reduce, sum } from 'lodash';
+import * as util from '../../../util/util';
+import * as test from '../../../util/test';
+import chalk from 'chalk';
+import { log, logSolution, trace } from '../../../util/log';
+import { performance } from 'perf_hooks';
 
 const YEAR = 2023;
 const DAY = 3;
@@ -12,12 +12,78 @@ const DAY = 3;
 // data path    : /home/rothanak/Projects/advent-of-code-2023/years/2023/03/data.txt
 // problem url  : https://adventofcode.com/2023/day/3
 
+function filterAdjacentNumbers(input: string): number[] {
+	const rows = input.split('\n');
+	const filteredNumbers: number[] = [];
+
+	rows.forEach((row, rowIndex) => {
+		let colIndex = 0;
+		while (colIndex < row.length) {
+			const char = row[colIndex];
+			if (char >= '0' && char <= '9') {
+				const numDigits = getNumberOfDigits(row, colIndex);
+				const isAdjacentToSymbol = checkAdjacency(rows, rowIndex, colIndex, numDigits);
+
+				// Add the number to the filtered array if adjacent to a symbol
+				if (isAdjacentToSymbol) {
+					filteredNumbers.push(Number(row.substring(colIndex, colIndex + numDigits)));
+				}
+
+				colIndex += numDigits;
+			} else {
+				colIndex++;
+			}
+		}
+	});
+
+	return filteredNumbers;
+}
+
+function checkAdjacency(rows: string[], rowIndex: number, colIndex: number, numDigits: number): boolean {
+	const maxRowIndex = rows.length - 1;
+	const maxColIndex = rows[0].length - 1;
+
+	for (let i = Math.max(0, rowIndex - 1); i <= Math.min(maxRowIndex, rowIndex + 1); i++) {
+		for (let j = Math.max(0, colIndex - 1); j <= Math.min(maxColIndex, colIndex + numDigits); j++) {
+			const current = rows[i][j];
+			if (
+				current !== '.' &&
+				!(i === rowIndex && j >= colIndex && j < colIndex + numDigits) &&
+				!(typeof +current !== 'number') &&
+				current !== undefined
+			) {
+				console.log('🚀 ~ file: index.ts:49 ~ checkAdjacency ~ current:', current);
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function getNumberOfDigits(row: string, colIndex: number): number {
+	let numDigits = 1;
+	for (let i = colIndex + 1; i < row.length; i++) {
+		if (row[i] >= '0' && row[i] <= '9') {
+			numDigits++;
+		} else {
+			break;
+		}
+	}
+	return numDigits;
+}
+
 async function p2023day3_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const filteredOutput = filterAdjacentNumbers(input);
+	console.log(
+		'🚀 ~ file: index.ts:75 ~ p2023day3_part1 ~ filteredOutput:',
+		filteredOutput[filteredOutput.length - 1]
+	);
+	return sum(filteredOutput);
 }
 
 async function p2023day3_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	return 'Not implemented';
 }
 
 async function run() {
@@ -45,13 +111,13 @@ async function run() {
 	const part1Solution = String(await p2023day3_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2023day3_part2(input));
 	const part2After = performance.now();
 
 	logSolution(3, 2023, part1Solution, part2Solution);
 
-	log(chalk.gray("--- Performance ---"));
+	log(chalk.gray('--- Performance ---'));
 	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
 	log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
 	log();
